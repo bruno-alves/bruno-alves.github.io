e759969f28562dbb3982cbbdca630937035cd53e
Bruno Alves <bruno@smn.com.br>
2020-06-17 13:42:57 -0300

    adicionando iframe e nova maneira de troca de click no botão de play

--- newhome/index.js
+++ newhome/index.js
@@ -1,1 +1,9 @@
-function changeImage(img, path) { $(img).attr('src', path) }
+$('div[class="buttonPlay"] img')
+    .on('mousedown', function() {
+        $(this).hide();
+        $(this).next().show();
+    })
+    .on('mouseup', function() {
+        $(this).hide();
+        $(this).prev().show();
+    })
