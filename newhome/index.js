100cbf1cc34ad60535a874343e5dced7a09c9f98
Bruno Alves <bruno@smn.com.br>
2020-06-19 19:39:49 -0300

    click dos botões do menu

--- newhome/index.js
+++ newhome/index.js
@@ -1,9 +1,19 @@
 $('div[class="buttonPlay"] img')
-    .on('mousedown', function() {
-        $(this).hide();
-        $(this).next().show();
+    .on('mousedown', function(e) {
+        if (e.which == 1) {
+            $(this).hide();
+            $(this).next().attr('pressed', true).show();
+        }
     })
-    .on('mouseup', function() {
-        $(this).hide();
-        $(this).prev().show();
+    .on('mouseup', function(e) {
+        if (e.which == 1 && $(this).attr('pressed')) {
+            $(this).hide().removeAttr('pressed');
+            $(this).prev().show();
+        }
+    })
+    .on('mouseleave', function() {
+        if ($(this).attr('pressed')) {
+            $(this).hide();
+            $(this).prev().show();
+        }
     })
