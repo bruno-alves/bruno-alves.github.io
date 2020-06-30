a63848d9dcdacd3d1e18d798f0db8c6783409a45
Bruno Alves <bruno@smn.com.br>
2020-06-30 19:36:42 -0300

    novo inimigo

--- scripts/newgame/control.js
+++ scripts/newgame/control.js
@@ -1,4 +1,4 @@
-let scenario, character, enemyDroplet, soundTrack, jumpingSound
+let scenario, character, enemyDroplet, enemyTroll, soundTrack, jumpingSound
 
 function preload() {
     soundTrack = loadSound('assets/newgame/sounds/soundtrack.mp3');
@@ -12,7 +12,10 @@ function setup() {
     scenario = new Scenario(loadImage('assets/newgame/images/forest.png'), 2);
     character = new Character(loadImage('assets/newgame/images/character.png'), 4, 4, 0, height - 135, 110, 135, 0, 0, 220, 270);
     enemyDroplet = new Enemy(loadImage('assets/newgame/images/droplet.png'), 4, 7, width - 60, height - 60, 60, 60, 0, 0, 104, 104, 10);
-    soundTrack.loop();
+
+    enemyTroll = new Enemy(loadImage('assets/newgame/images/troll.png'), 5, 6, width - 200, height - 200, 200, 200, 0, 0, 400, 400, 10);
+
+    //soundTrack.loop();
 }
 
 function keyPressed() {
@@ -26,11 +29,14 @@ function draw() {
     scenario.show();  
     character.show();
     character.gravity();
-
+    
     enemyDroplet.show();
     enemyDroplet.move();
 
+    enemyTroll.show();
+    enemyTroll.move();
+
     if (character.isCollidingWithEnemy(enemyDroplet)) {
-        noLoop();
+       // noLoop();
     }
 }
